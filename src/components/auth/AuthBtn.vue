93fe2a3009ba10251d2918136702083af3baf54c
eomsue <todayilearn@naver.com>
2022-05-19 22:51:39 +0900

    첫 커밋

---
+++ src/components/auth/AuthBtn.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { initializeApp } from 'firebase/app'
+import firebaseConfig from '../../../firebaseConfig'
+import { getAuth, connectAuthEmulator, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+
+// :firebase 초기화
+initializeApp(firebaseConfig)
+
+// :에뮬레이터 설정
+const auth = getAuth()
+auth.useDeviceLanguage()
+connectAuthEmulator(auth, 'http://localhost:9099')
+
+// :구글 로그인 인증
+const provider = new GoogleAuthProvider()
+
+</script>
+<template>
+  <q-btn round color="info" @click="signInWithPopup(auth, provider)">
+    <q-avatar
+      icon="mdi-login"
+      size="32px"
+    />
+  </q-btn>
+</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AuthBtn from 'components/auth/AuthBtn.vue'
+</script>
+<template>
+  <q-layout
+    view="hhh lpr fff"
+    :container="false"
+  >
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-title>
+          vf3
+        </q-toolbar-title>
+        <AuthBtn />
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
